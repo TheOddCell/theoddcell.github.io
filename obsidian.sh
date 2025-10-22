d225cce9c24a9fa1b6035366ffe11ba39c2dc228
/home/odd <odd@obsidianos.xyz>
2025-10-22 19:52:56 -0400

    Fix execution command in obsidian.sh

--- obsidian.sh
+++ obsidian.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-exec bash -c "$(curl -s https://gist.github.com/TheOddCell/cfa91a77dc7214ad2f97656633a4911a)"
+bash -c "$(curl -s https://gist.github.com/TheOddCell/cfa91a77dc7214ad2f97656633a4911a)"
