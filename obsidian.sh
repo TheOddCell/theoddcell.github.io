fbbb5d84d6bd47eb0a4007252cf07d058305d0ce
/home/odd <odd@obsidianos.xyz>
2025-10-23 14:04:30 -0400

    Update obsidian.sh

--- obsidian.sh
+++ obsidian.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-bash -c "$(curl -s https://gist.github.com/TheOddCell/cfa91a77dc7214ad2f97656633a4911a)"
+temp=$(mktemp)
+curl -s https://gist.github.com/TheOddCell/cfa91a77dc7214ad2f97656633a4911a > $temp
+exec bash $temp
