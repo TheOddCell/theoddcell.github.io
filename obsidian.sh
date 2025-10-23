b17f8ebb8947bd3c37416cba28648894f7260bcc
/home/odd <odd@obsidianos.xyz>
2025-10-23 14:06:29 -0400

    fuck

--- obsidian.sh
+++ obsidian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 temp=$(mktemp)
-curl -s https://gist.github.com/TheOddCell/cfa91a77dc7214ad2f97656633a4911a > $temp
+curl -s https://gist.githubusercontent.com/TheOddCell/cfa91a77dc7214ad2f97656633a4911a/raw/f07ad817f53e9b2515e0c7838de20623df4523a4/obsidian.sh > $temp
 exec bash $temp
